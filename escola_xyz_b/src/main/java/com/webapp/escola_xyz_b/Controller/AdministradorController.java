cf0f79dbaddd17097e488cdabcb4d7c58e2521ee
Diogotb <digogo@gmail.com>
2024-03-28 18:33:14 -0300

    28/03

---
+++ escola_xyz_b/src/main/java/com/webapp/escola_xyz_b/Controller/AdministradorController.java
@@ -0,0 +1,46 @@
+package com.webapp.escola_xyz_b.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+import com.webapp.escola_xyz_b.Model.Administrador;
+import com.webapp.escola_xyz_b.Repository.AdministradorRepository;
+import com.webapp.escola_xyz_b.Repository.VerificaCadastroAdmRepository;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+@Controller
+public class AdministradorController {
+
+    @Autowired
+    AdministradorRepository ar;
+
+    @Autowired
+    VerificaCadastroAdmRepository vcar;
+    
+    @PostMapping("cadastrar-adm")
+    public String cadastrarAdmBD(Administrador adm) {
+        boolean verificaCpf = vcar.existsById(adm.getCpf());
+        if(verificaCpf){
+            ar.save(adm);
+            System.out.println("Cadastro Realizado com Sucesso");
+        }else{
+            System.out.println("Falha ao Cadastrar");
+
+        }
+        
+        return "/login/login-adm";
+    }
+    @PostMapping("acesso-adm")
+    public String postMethodName(@RequestBody String entity) {
+        //metodo para verificar acesso
+        
+        
+        return entity;
+    }
+    
+    
+}
